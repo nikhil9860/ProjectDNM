d5d7a4ed49b6dce52d8e1dd287a8f1580690a237
nikhil <nikhil9860@gmail.com>
2018-02-22 18:56:47 +0530

    DataBase Handeler added

---
+++ src/java/com/java/DataBase/DataBaseHandler.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.java.DataBase;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author nikhil
+ */
+public class DataBaseHandler {
+    
+    private static  Connection con;
+    
+    public static Connection  getConnection(){
+        
+        try {
+                
+            Class.forName("com.mysql.jdbc.Driver");
+            
+            con = DriverManager.getConnection("jdbc:mysql://localhost/DNM","root","");
+            
+            
+            
+            return con ;
+            
+        } catch (SQLException ex) {
+            Logger.getLogger(DataBaseHandler.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(DataBaseHandler.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        return null;
+        
+    }
+    
+}
