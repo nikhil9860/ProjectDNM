78d6a59d5d0f88cfdde7a228bb14af0f1a78ba30
nikhil <nikhil9860@gmail.com>
2018-04-18 14:16:17 +0530

    Done Appointment updated

--- src/java/com/java/doctor/classfiles/DoneAppointment.java
+++ src/java/com/java/doctor/classfiles/DoneAppointment.java
@@ -8,22 +8,31 @@ package com.java.doctor.classfiles;
 import com.java.DataBase.DoctorDataBaseHandler;
 import com.opensymphony.xwork2.ActionSupport;
 import java.sql.SQLException;
+import java.util.Map;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 import org.apache.struts2.interceptor.ServletRequestAware;
+import org.apache.struts2.interceptor.SessionAware;
 
 /**
  *
  * @author nikhil
  */
-public class DoneAppointment extends ActionSupport implements ServletRequestAware{
+public class DoneAppointment extends ActionSupport implements ServletRequestAware,SessionAware{
     
+    Map<String, Object> session;
     HttpServletRequest req;
-    
+    String doctor_uname;
     String patient_name;
     String appointment_date;
     
+   
+     @Override
+    public void setSession(Map<String, Object> map) {
+        session= map;
+    }
     
     @Override
     public void setServletRequest(HttpServletRequest hsr) {
@@ -32,11 +41,12 @@ public class DoneAppointment extends ActionSupport implements ServletRequestAwar
     public  String execute(){
         
         patient_name= req.getParameter("patient_name");
-        
+        doctor_uname=session.get("uname").toString();
         appointment_date = req.getParameter("appointment_date");
         
         
-        String sql = "UPDATE Appointments inner JOIN Patient ON Appointments.patient_id=Patient.patient_id SET Appointments.status = 'done' WHERE Patient.patient_name='"+patient_name+"' AND Appointments.appointment_date='"+appointment_date+"'";
+        String sql = "UPDATE Patient inner JOIN Doctors ON Patient.doctor_id=Doctors.doctor_id SET Patient.status ='done' WHERE Patient.patient_name='"+patient_name+"' AND Patient.appointment_date = '"+appointment_date+"' AND Doctors.doctor_uname='"+doctor_uname+"'";
+                        
         
         try {
             DoctorDataBaseHandler.getConnection().createStatement().executeUpdate(sql);
@@ -48,6 +58,8 @@ public class DoneAppointment extends ActionSupport implements ServletRequestAwar
         
         return SUCCESS;
     }
+
+   
 
     
     
